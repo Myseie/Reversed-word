5e11d269aa97e03d2e1081e4ae4bbb72dcf36bb3
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:18:03 +0200

    Add files via upload

---
+++ Reversed word/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Skriv en mening här :");
+
+string input = Console.ReadLine();
+
+string[] words = input.Split();
+
+Array.Reverse(words);
+
+Console.WriteLine("Omvänd mening : ");
+Console.WriteLine(string.Join(" ", words));
